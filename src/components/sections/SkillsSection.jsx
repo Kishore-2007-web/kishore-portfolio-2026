7e51263a58d3220292bd71460d5d94fcb25d7ecb
Kishore <itsmekishore00@gmail.com>
2026-09-13 10:33:58 +0530

    Updated project

--- src/components/sections/SkillsSection.jsx
+++ src/components/sections/SkillsSection.jsx
@@ -92,7 +92,7 @@ export function SkillsSection() {
         >
           <InfiniteSpiral
             items={skillItems}
-            animationMode="all"
+            animationMode="auto"
             speed={0.8}
             radius={180}
             cardWidth={120}
